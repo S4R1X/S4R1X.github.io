a62a965a3a58296b0a303500ccd87bfde704034e
SirL3X0N <seth@bytech.ca>
2024-07-14 01:38:53 +0200

    fixed pre being clickable when rules are open

--- Js/Pre.js
+++ Js/Pre.js
@@ -1,11 +1,13 @@
 function closePre(starter) {
-    sym = starter
-    document.getElementById('board').classList.remove('going')
-    document.getElementById('pre').classList.add('going')
-    let boards = document.getElementsByClassName('subboard')
-    for (let index = 0; index < boards.length; index++) {
-        if (index != 4) {
-            boards[index].classList.add('b' + sym)
+    if (rules === false) {
+        sym = starter
+        document.getElementById('board').classList.remove('going')
+        document.getElementById('pre').classList.add('going')
+        let boards = document.getElementsByClassName('subboard')
+        for (let index = 0; index < boards.length; index++) {
+            if (index != 4) {
+                boards[index].classList.add('b' + sym)
+            }
         }
     }
 }
--- Js/Rules.js
+++ Js/Rules.js
@@ -1,10 +1,12 @@
 function openRules() {
     document.getElementById('rules').classList.remove('going')
+    document.getElementById('pre').classList.add('going')
     document.getElementById('rulesIcon').classList.add('going')
     rules = !rules
 }
 function closeRules() {
     document.getElementById('rulesIcon').classList.remove('going')
     document.getElementById('rules').classList.add('going')
+    document.getElementById('pre').classList.remove('going')
     rules = !rules
 }
