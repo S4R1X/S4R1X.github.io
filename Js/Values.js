9ee3745c4b855e552bbb679dcf5d52b346da1c41
SirL3X0N <seth@bytech.ca>
2024-07-25 22:28:43 +0200

    removed unused pre value

--- Js/Values.js
+++ Js/Values.js
@@ -6,7 +6,6 @@ sym = null
 won = false
 target = 10
 pos = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
-pre = false
 firstTurn = '4'
 rules = false
 const Myhtml = document.getElementById('body').innerHTML
