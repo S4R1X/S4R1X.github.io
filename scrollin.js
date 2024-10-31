722075924108a7e96fb530d02fa3f57c090dad8b
SirL3X0N <seth@bytech.ca>
2024-10-31 17:57:20 +0100

    Now my personal website

--- scrollin.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function () {
-    console.log(window.scrollY)
-    const textBox = document.getElementById('aboutbox');
-    if (window.scrollY > 0) {
-        textBox.classList.add('visible'); // Add visibility when scrolled down 100px
-    } else {
-        textBox.classList.remove('visible');
-    }
-});
