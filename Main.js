aeac345622b218b2a851b17f2e9009bbe3478c98
SirL3X0N <seth@bytech.ca>
2024-11-01 15:24:26 +0100

    Dropedown menu

--- Main.js
+++ Main.js
@@ -27,3 +27,34 @@ const closeBtn = document.querySelector('#closemodel')
 
 openBtn.addEventListener('click', () => dialog.showModal())
 closeBtn.addEventListener('click', () => dialog.close())
+
+
+const hamburgerBtn = document.querySelector('#Hamburger')
+const upBtn = document.querySelector('#backUp')
+
+hamburgerBtn.addEventListener('click', () => dropDownToggle())
+upBtn.addEventListener('click', () => dropDownToggle())
+
+
+let dropOpen = false;
+const dropMen = document.getElementById("dropper");
+
+function dropDownToggle() {
+
+    console.log('hit')
+    dropOpen = !dropOpen;
+
+    if (dropOpen) {
+        console.log(dropOpen)
+        dropMen.classList.add("show");
+    } else {
+        dropMen.classList.remove("show");
+        console.log(dropOpen)
+
+    }
+
+    console.log(dropMen.classList)
+}
+
+
+// hamburgerBtn.addEventListener('click', () => )
