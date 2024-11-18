c16035da29ebd72fddd3a87f2c271712cba4f687
SirL3X0N <seth@bytech.ca>
2024-11-18 15:35:38 +0100

    Moved Hamburger to own file

--- Main.js
+++ Main.js
@@ -19,30 +19,3 @@ window.addEventListener("scroll", () => {
 
     lastScrollPosition = currentScrollPosition;
 });
-
-const hamburgerBtn = document.querySelector('#Hamburger')
-const upBtn = document.querySelector('#backUp')
-
-hamburgerBtn.addEventListener('click', () => dropDownToggle())
-upBtn.addEventListener('click', () => dropDownToggle())
-
-
-let dropOpen = false;
-const dropMen = document.getElementById("dropper");
-
-function dropDownToggle() {
-
-    console.log('hit')
-    dropOpen = !dropOpen;
-
-    if (dropOpen) {
-        console.log(dropOpen)
-        dropMen.classList.add("show");
-    } else {
-        dropMen.classList.remove("show");
-        console.log(dropOpen)
-
-    }
-
-    console.log(dropMen.classList)
-}
