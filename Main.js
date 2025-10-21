3c4a6c22ee4c6943f68a7e557f40b1e2807d2fe6
SirL3X0N <seth@bytech.ca>
2025-10-21 21:09:09 +0200

    Just a few changes, now using bootstrap

--- Main.js
+++ Main.js
@@ -1,21 +1,5 @@
-document.querySelector('.callink').addEventListener('click', function (event) {
-    event.preventDefault();
-    document.querySelector('#calendar').scrollIntoView({ behavior: 'smooth' });
-});
-
-let lastScrollPosition = 0;
-const header = document.getElementById("header");
-
-window.addEventListener("scroll", () => {
-    const currentScrollPosition = window.pageYOffset;
-
-    if (currentScrollPosition > lastScrollPosition) {
-        // Scrolling down: hide the header
-        header.classList.add("hidden");
-    } else if (currentScrollPosition < lastScrollPosition) {
-        // Scrolling up: show the header
-        header.classList.remove("hidden");
-    }
-
-    lastScrollPosition = currentScrollPosition;
-});
+fetch('nav.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar-placeholder').innerHTML = data;
+  });
