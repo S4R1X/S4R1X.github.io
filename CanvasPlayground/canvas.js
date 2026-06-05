2506f247a9ca5bf106c810baacb2d807f8800991
SirL3X0N <seth@bytech.ca>
2026-06-05 15:31:47 +0200

    Lots of playing around :)

--- CanvasPlayground/canvas.js
+++ CanvasPlayground/canvas.js
@@ -1,50 +1,60 @@
 var canvas = document.querySelector("canvas");
 
 const clPrimary = window.getComputedStyle(document.body).getPropertyValue('--cl-primary');
-
+const clSecondary = window.getComputedStyle(document.body).getPropertyValue('--cl-secondary');
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight;
 
+function hexToRgb(hex) {
+    hex = hex.replace('#', '');
+
+    return {
+        r: parseInt(hex.substring(0, 2), 16),
+        g: parseInt(hex.substring(2, 4), 16),
+        b: parseInt(hex.substring(4, 6), 16)
+    };
+}
+
+function randomColorOnGradient(c1, c2) {
+    const t = Math.random();
+
+    return {
+        r: Math.round(c1.r + (c2.r - c1.r) * t),
+        g: Math.round(c1.g + (c2.g - c1.g) * t),
+        b: Math.round(c1.b + (c2.b - c1.b) * t),
+    };
+}
+
 var c = canvas.getContext("2d");
 
-// c.fillStyle = "#5fdab352";
-// c.fillRect(100, 100, 100, 100);
-// c.fillStyle = "#5fdab390";
-// c.fillRect(300, 200, 100, 100);
-// c.fillStyle = "#5fdab322";
-// c.fillRect(200, 400, 100, 100);
+var mouse = {
+    x: undefined,
+    y: undefined
+}
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x
+    mouse.y = event.y
+})
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight;
+    init();
+})
 
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-// c.strokeStyle = clPrimary;
-// c.stroke();
-
-// for (var i = 0; i < 700; i++) {
-//     var x = Math.random() * window.innerWidth;
-//     var y = Math.random() * window.innerHeight;
-//     var r = Math.floor(Math.random() * 256);
-//     var g = Math.floor(Math.random() * 256);
-//     var b = Math.floor(Math.random() * 256);
-//     var color = 'rgb(' + r + ',' + g + ',' + b + ')';
-//     c.beginPath();
-//     c.arc(x, y, 30, 0, Math.PI * 2, false);
-//     c.strokeStyle = color;
-//     c.stroke();
-// }
-
-function Circle(x,y,dx,dy,radius,green){
+function Circle(x,y,dx,dy,radius, red, green, blue){
     this.x = x
     this.y = y
     this.dx = dx
     this.dy = dy
     this.radius = radius
+    this.inflate = 0
+    this.red = red;
     this.green = green;
+    this.blue = blue;
     this.draw = function () {
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.fillStyle = 'rgb(256, ' + this.green + ', 256)';
+        c.arc(this.x, this.y, this.radius + this.inflate, 0, Math.PI * 2, false);
+        c.fillStyle = 'rgb(' + this.red + ', ' + this.green + ', ' + this.blue + ')';
         c.fill();
     }
     this.update = function () {
@@ -57,6 +67,14 @@ function Circle(x,y,dx,dy,radius,green){
         }
         this.x += this.dx;
         this.y += this.dy;
+
+        if (mouse.x - this.x < (this.radius + this.inflate) && mouse.x - this.x > -(this.radius + this.inflate) && mouse.y - this.y < (this.radius + this.inflate) && mouse.y - this.y > -(this.radius + this.inflate)) {
+            if (this.inflate < this.radius ) {
+                this.inflate += 1
+            }
+        }else if(this.inflate > 0 ) {
+            this.inflate -= 1
+        }
         this.draw();
     }
 }
@@ -72,17 +90,22 @@ if (window.innerWidth > window.innerHeight) {
 }
 
 screen_size = (A - B)/20
-
 var circle_array = [];
-for (var i = 0; i < 100; i++) {
+
+function init(){
+    circle_array = [];
+for (var i = 0; i < 75; i++) {
     var radius = Math.random() * screen_size + 25
     var x = Math.random() * (window.innerWidth - radius * 2) + radius   
     var y = Math.random() * (window.innerHeight - radius * 2) + radius
     var dx = (Math.random() - 0.5) * 2
     var dy = (Math.random() - 0.5) * 2
-    var green = Math.floor((Math.random()+2) * 85);
-    circle_array.push(new Circle(x, y, dx, dy, radius,green));
-}
+    const { r, g, b } = randomColorOnGradient(hexToRgb(clPrimary), hexToRgb(clSecondary));
+
+    circle_array.push(
+        new Circle(x, y, dx, dy, radius, r, g, b)
+    );
+}}
 
 function animate_circle() {
     requestAnimationFrame(animate_circle);
@@ -93,3 +116,4 @@ function animate_circle() {
 }
     
 animate_circle();
+init()
